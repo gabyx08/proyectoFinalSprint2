bba6066a7b5edd0a08bc8919cc76d5ed490abb15
fbk-rena <gaby.ranarz@gmail.com>
2017-03-30 13:05:27 -0600

    borra lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,41 @@
+
 function crearLista(){
   var nombre = document.getElementById("nombreNuevaLista").value;
   var nuevaLista = document.createElement("UL");
   nuevaLista.appendChild(document.createTextNode(nombre));
-  nuevaLista.setAttribute("id","hola");
-
+  nuevaLista.setAttribute("id","lista");
   var textoPendientes = document.createElement("INPUT");
   textoPendientes.setAttribute("id","nombreNuevoPendiente");
   var botonPendientes = document.createElement("INPUT");
   botonPendientes.setAttribute("type","button");
   botonPendientes.setAttribute("value","Crear Pendiente");
+  botonPendientes.setAttribute("id","botonPendi");
   botonPendientes.setAttribute("onclick","agregarPendiente()");
+  var botonBorrarLista= document.createElement("INPUT")
+  botonBorrarLista.setAttribute("type","button");
+  botonBorrarLista.setAttribute("value","Borrar Lista");
+  botonBorrarLista.setAttribute("id","botonBorrarLi");
+  botonBorrarLista.setAttribute("onclick","borrarLista()");
   var separador= document.createElement("HR");
+  separador.setAttribute("id","hr");
   document.getElementById("agregarListas").appendChild(nuevaLista);
   document.getElementById("agregarListas").appendChild(textoPendientes);
   document.getElementById("agregarListas").appendChild(botonPendientes);
+  document.getElementById("agregarListas").appendChild(botonBorrarLista);
   document.getElementById("agregarListas").appendChild(separador);
 }
 
 function agregarPendiente(){
   var nombrePendiente= document.getElementById("nombreNuevoPendiente").value;
-  console.log(nombrePendiente);
   var nuevoPendiente= document.createElement("LI");
   nuevoPendiente.appendChild(document.createTextNode(nombrePendiente));
-  document.getElementById("hola").appendChild(nuevoPendiente);
+  document.getElementById("lista").appendChild(nuevoPendiente);
 }
-//<nodo>.setAttribute(<atributo>,<valor>);
+
+function borrarLista(){
+document.getElementById("agregarListas").removeChild(document.getElementById("lista"));
+document.getElementById("agregarListas").removeChild(document.getElementById("nombreNuevoPendiente"));
+document.getElementById("agregarListas").removeChild(document.getElementById("botonPendi"));
+document.getElementById("agregarListas").removeChild(document.getElementById("botonBorrarLi"));
+document.getElementById("agregarListas").removeChild(document.getElementById("hr"));
+}
